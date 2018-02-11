221291d8b176088a8e36490689ca798e17fb5284
Yun Zhou <zhou0360@umn.edu>
2018-02-11 16:11:42 -0600

    Initial Commit

---
+++ ann-mod1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+    Sub Main()
+        Dim myNum As Double
+        Dim myMod As Integer
+        Dim myString As String = "Hello World"
+        Dim myBool As Boolean = 5 > 9
+        Dim myAns As Double = Nothing
+
+        myNum = 5 / 2
+        myMod = 13 Mod 5
+        myAns = myNum * myMod
+
+        'This line writes the text hello world to the console'
+        Console.WriteLine(myNum.ToString() & " - " & myMod & " - " & myAns & " - " & myString & " - " & myBool)
+
+        'This line pauses the program and lets the user read the text'
+        Console.ReadLine()
+
+    End Sub
+End Module
